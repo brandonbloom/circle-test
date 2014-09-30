b1957672ccab810e618261c14d345592f0bae90e
Brandon Bloom <brandon@brandonbloom.name>
2014-09-30 16:13:45 -0400

    testing some artifact stuff

--- test/test.js
+++ test/test.js
@@ -1,4 +1,14 @@
 var assert = require("assert")
+var execSync = require('exec-sync'); // because I'm lazy
+
+execSync('mkdir build-blah/');
+execSync('touch build-blah/abc');
+
+execSync('mkdir ~/home-blah');
+execSync('touch ~/home-blah/xyz');
+
+execSync('touch file-blah');
+
 describe('Array', function(){
   describe('#indexOf()', function(){
     it('should return -1 when the value is not present', function(){
